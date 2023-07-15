2348deb9df2b21c81393b6290fc2432138f9cc26
jaeyoungjang2 <lion85488@gmail.com>
2023-07-15 21:43:25 +0900

    feat

---
+++ src/main/java/com/example/springdataredis/jackson2hashmapper/Address.java
@@ -0,0 +1,11 @@
+package com.example.springdataredis.jackson2hashmapper;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Address {
+  String city;
+  String country;
+}
